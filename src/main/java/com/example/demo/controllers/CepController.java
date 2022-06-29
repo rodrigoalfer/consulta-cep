f6d9f2e99570f9c21c0da0394382c559c77d0603
Rodrigo Alberto Ferreira <107878313+rodrigoalfer@users.noreply.github.com>
2022-06-29 20:25:26 -0300

    modifica o controller

--- src/main/java/com/example/demo/controllers/CepController.java
+++ src/main/java/com/example/demo/controllers/CepController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.example.demo.models.Cep;
 import com.example.demo.services.CepService;
@@ -16,12 +20,12 @@ public class CepController {
 	private CepService cepService;
 	
 	@GetMapping("/{cep}")
-	public Cep consultarCep (String cep) {
+	public ResponseEntity<Cep> consultarCep (@PathVariable("cep") String cep) {
 	
 		Cep cepConsulta = cepService.consultarCep(cep);
 		if(cepConsulta!=null) {
-			return cepConsulta;
+			return ResponseEntity.ok().body(cepConsulta);
 		}
-		return null;
+		throw new ResponseStatusException(HttpStatus.NOT_FOUND);
 	}
 }
