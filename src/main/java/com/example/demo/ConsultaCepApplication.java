d928af70dccb633e59c8ca354aaafbc0657b60dd
Rodrigo Alberto Ferreira <107878313+rodrigoalfer@users.noreply.github.com>
2022-06-29 19:13:57 -0300

    Initial commit

---
+++ src/main/java/com/example/demo/ConsultaCepApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaCepApplication.class, args);
+	}
+
+}
